f861be676a65848527301d6fcf40d49fa21b5a5d
ElizabethAdams1 <beekeeper01@gmail.com>
2017-04-21 13:05:21 -0600

    multiple lines on graph

--- project4/public/js/stockTrends.js
+++ project4/public/js/stockTrends.js
@@ -1,5 +1,5 @@
 $(function () {
-    var chart = Highcharts.chart({
+    var linechart = Highcharts.chart({
         chart: {
             renderTo: 'container',
             plotBackgroudColor: {
@@ -23,34 +23,85 @@ $(function () {
                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
              },
         // },
-        plotOptions: {
-            series: {
-                color: '#FF0000'
-                }
-            },
+        // plotOptions: {
+        //     series: {
+        //         color: '#FF0000'
+        //         }
+        //     },
         series: [{
-            data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+            color: 'red',
+            name: 'Firefox',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        }, {
+            color: 'yellow',
+            name: 'IE',
+            data: [10, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+        }, {
+            color: 'blue',
+            name: 'Chrome',
+            data: [5, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+        }, {
+            color: 'pink',
+            name: 'Safari',
+            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+        }, {
+            color: 'violet',
+            name: 'Opera',
+            data: [7.9, 5.2, 2.7, 9.5, 1.9, 5.2, 7.0, 6.6, 4.2, 1.3, 16.6, 14.8]
+        }, {
+            color: 'green',
+            name: 'Others',
+            data: [15, 10.6, 13.5, 18.4, 3.5, 7.0, 8.6, 7.9, 4.3, 19.0, 13.9, 11.0]
         }]
-            // line: {
-            //     allowPointSelect: true,
-            //     cursor: 'pointer',
-            //     dataLabels:{
-            //         enabled: true
-            //     },
-            //     showInLegend: true
-            // }
-        // },
         // series: [{
-        //     type: 'line',
-        //     name: 'test',
-        //     data: [
-        //         ['Firefox', 10.0],
-        //         ['IE', 10],
-        //         ['Chrome', 10],
-        //         ['Safari', 10],
-        //         ['Opera', 10],
-        //         ['Others', 50]
-        //     ]
+        //     data: [15.9, 12.5, 114.4, 18.2, 135.0, 154.0, 110.6, 87.5, 216.4, 194.1, 95.6, 54.4],
         // }]
-    })
+
+    });
+    // var piechart = new Highcharts.Chart({
+    //     chart: {
+    //         renderTo: 'container',
+    //         plotBackgroundColor: null,
+    //         plotBorderWidth: null,
+    //         plotShadow: false,
+    //         events: {
+    //             redraw: function () {
+    //                 //update sliders   
+    //                 $.each(chart.series[0].points, function (i, point) {
+    //                     console.log(point.slider, point);
+    //                     point.slider.slider('value', point.percentage);
+    //                 });
+    //             }
+    //         }
+    //     },
+    //     title: {
+    //         text: 'Browser market shares at a specific website, 2010'
+    //     },
+    //     tooltip: {
+    //         pointFormat: '{series.name}: <b>{point.percentage}%</b>',
+    //         percentageDecimals: 1
+    //     },
+    //     plotOptions: {
+    //         pie: {
+    //             allowPointSelect: true,
+    //             cursor: 'pointer',
+    //             dataLabels: {
+    //                 enabled: false
+    //             },
+    //             showInLegend: true
+    //         }
+    //     },
+    //     series: [{
+    //         type: 'pie',
+    //         name: 'Browser share',
+    //         data: [
+    //             ['Firefox', 10.0],
+    //             ['IE', 10],
+    //             ['Chrome', 10],
+    //             ['Safari', 10],
+    //             ['Opera', 10],
+    //             ['Others', 50]
+    //         ]
+    //     }]
+    // });
 });
