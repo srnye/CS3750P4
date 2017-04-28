a481103acc8df8b42518e5c6fd813fd14f53a14a
srnye <steven.r.nye@gmail.com>
2017-04-27 22:30:18 -0600

    Changes to view stocks

--- project4/public/js/stocksView.js
+++ project4/public/js/stocksView.js
@@ -1,4 +1,16 @@
 $(function () {
+    //define local variables
+    var dayBtn = document.getElementById("dayBtn");
+    var weekBtn = document.getElementById("weekBtn");
+    var monthBtn = document.getElementById("monthBtn");
+    var stockData = document.getElementById("stockData");
+    var categories = document.getElementById("categories");
+    var timeIntv = document.getElementById("timeIntv");
+    var intvForm = document.getElementById("intvForm");
+    var stocks = document.getElementById("ogStocks");
+
+    stocks = JSON.parse(stocks.value);
+
     var linechart = Highcharts.chart({
         chart: {
             renderTo: 'container',
@@ -18,7 +30,7 @@ $(function () {
             pointFormat: '{series.name}: <b>{point.y}</b><br/>',
         },
         xAxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        categories: JSON.parse(categories.value)
         },
         plotOptions: {
                 series: {
@@ -47,33 +59,28 @@ $(function () {
                     }
                 }
             },        
-        series: [{
-            // color: 'red',
-            name: 'Firefox',
-            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-        }, {
-            // color: 'yellow',
-            name: 'IE',
-            data: [10, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-        }, {
-            // color: 'blue',
-            name: 'Chrome',
-            data: [5, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-        }, {
-            // color: 'pink',
-            name: 'Safari',
-            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-        }, {
-            // color: 'violet',
-            name: 'Opera',
-            data: [7.9, 5.2, 2.7, 9.5, 1.9, 5.2, 7.0, 6.6, 4.2, 1.3, 16.6, 14.8]
-        }, {
-            // color: 'green',
-            name: 'Others',
-            data: [15, 10.6, 13.5, 18.4, 3.5, 7.0, 8.6, 7.9, 4.3, 19.0, 13.9, 11.0]
-        }]
+        series: JSON.parse(stockData.value)
     });
-    var piechart = Highcharts.chart({
+
+    var tempStock = [];
+    var reserve = 100;
+
+    for(var stock in stocks)
+    {
+        var s = stocks[stock];
+        var item = [];       
+        item.push(s.name);
+        item.push(s.percentage);
+        tempStock.push(item);
+        reserve -= s.percentage;
+    }
+
+    var item = [];
+    item.push('Reserve');
+    item.push(reserve);
+    tempStock.push(item);
+
+    var chart = new Highcharts.Chart({
         chart: {
             renderTo: 'container_a',
             plotBackgroundColor: null,
@@ -90,7 +97,7 @@ $(function () {
             }
         },
         title: {
-            text: 'Browser market shares at a specific website, 2010'
+            text: 'Your Stock Percentages'
         },
         tooltip: {
             pointFormat: '{series.name}: <b>{point.percentage}%</b>',
@@ -106,18 +113,33 @@ $(function () {
                 showInLegend: true
             }
         },
-        
         series: [{
             type: 'pie',
-            name: 'Browser share',
-            data: [
-                ['Firefox', 10.0],
-                ['IE', 10],
-                ['Chrome', 10],
-                ['Safari', 10],
-                ['Opera', 10],
-                ['Others', 50]
-            ]
+            name: 'Stock Percentage',
+            data: tempStock
         }]
     });
+
+    //functions
+    dayBtn.onclick = function()
+    {
+        timeIntv.value = "day";
+
+        intvForm.submit();
+    }
+
+    weekBtn.onclick = function()
+    {
+        timeIntv.value = "week";
+
+        intvForm.submit();
+    }
+
+    monthBtn.onclick = function()
+    {
+        timeIntv.value = "month";
+
+        intvForm.submit();
+    }
+
 });
--- project4/routes/index.js
+++ project4/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var yahooFinance = require('yahoo-finance');
+var PathLoader = require('path-loader');
 
 const passport = require('../lib/auth').passport;
 const ensureAuthenticated = require('../lib/auth').ensureAuthenticated;
@@ -111,8 +112,315 @@ router.get('/myStocks', ensureAuthenticated, function(req, res, next) {
   //res.render('myStocks', { title: 'My Stocks', user: req.user, stocks: JSON.stringify(s)  });
 });
 
-router.get('/stockView', ensureAuthenticated, function(req, res, next){
-  res.render('stockView', {title: 'Stock View', user: req.user });
+router.post('/stocksView', (req, res, next) => 
+{
+  var s1 = [];
+
+  if (req.user.stocks.length > 0)
+  {
+    for(var stock in req.user.stocks)
+    {
+      if (isNaN(parseInt(stock)))
+      {    
+      }
+      else
+      {
+        s1.push(req.user.stocks[stock]);
+      }
+    }
+  }
+
+  const cat = req.body.timeIntv;
+
+  var s = [];
+  var date = new Date();
+  var todaysDate = convertDate(date);
+
+  if (req.user.stocks.length > 0)
+  {
+    for(var stock in req.user.stocks)
+    {
+      if (isNaN(parseInt(stock)))
+      {    
+      }
+      else
+      {
+        s.push(req.user.stocks[stock].symbol);
+      }
+    }
+
+    if (cat == "month")
+    {      
+      var lastDate = convertDateSubtractMonth(date);
+
+        yahooFinance.historical({
+        symbols: s,
+        from: lastDate,
+        to: todaysDate,
+        period: 'd',  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only) 
+      }, function (err, quotes) {
+        if (err)
+          {
+
+          }
+          else
+          {
+            //get categories
+            var cats = [];
+            for (var stock in quotes)
+            {
+              for (var d in quotes[stock])
+              {
+                cats.push(formatDateMMDD(quotes[stock][d].date));
+              }
+              break;
+            }
+            //format data
+            var data = [];
+            for (var stock in quotes)
+            {
+              var item = {name: quotes[stock][0].symbol, data: []};
+              //populate data
+              var tempData = [];
+              for (var d in quotes[stock])
+              {
+                tempData.push(quotes[stock][d].close);
+              }
+              item.data = tempData;
+              data.push(item);
+            }
+            res.render('stocksView', {title: 'Stocks View', user: req.user, stocks: data, ogStocks: s1, cats: cats });
+          } 
+      });
+    }
+    else if (cat == "day")
+    {
+
+      // //var test = readJsonFromUrl("http://dev.markitondemand.com/MODApis/Api/interactive?parameters=%7B%22Normalized%22:false,%22StartDate%22:%222013-07-15T00:00:00-00%22,%22EndOffsetDays%22:0,%22NumberOfDays%22:1,%22DataPeriod%22:%22Day%22,%22DataInterval%22:0,%22LabelPeriod%22:%22Hour%22,%22LabelInterval%22:1,%22ExtraPoints%22:0,%22Elements%22:%5B%7B%22Symbol%22:%22AAPL%22,%22ElementType%22:%22price%22,%22Params%22:%5B%22ohlc%22%5D%7D%5D,%22RealTime%22:false%7D");
+
+      //   //   jQuery.getJSON("http://dev.markitondemand.com/MODApis/Api/interactive?parameters=%7B%22Normalized%22:false,%22StartDate%22:%222013-07-15T00:00:00-00%22,%22EndOffsetDays%22:0,%22NumberOfDays%22:1,%22DataPeriod%22:%22Day%22,%22DataInterval%22:0,%22LabelPeriod%22:%22Hour%22,%22LabelInterval%22:1,%22ExtraPoints%22:0,%22Elements%22:%5B%7B%22Symbol%22:%22AAPL%22,%22ElementType%22:%22price%22,%22Params%22:%5B%22ohlc%22%5D%7D%5D,%22RealTime%22:false%7D?callback=?", function(data) {
+      //   // // Get the element with id summary and set the inner text to the result.
+      //   // console.log
+      //   //  });
+ 
+      //     var allData = [];   
+      //     //loop through other stocks
+      //     for(var i = 0; i < req.user.stocks.length; i++)
+      //     {
+      //        PathLoader
+      //       .load('https://chartapi.finance.yahoo.com/instrument/1.0/GOOG/chartdata;type=quote;range=1d/json')
+      //       .then(function (document) 
+      //       {   
+      //         document = document.replace('finance_charts_json_callback( ', '');
+      //         document = document.slice(0, -1);
+      //         console.log(document);
+      //         document = JSON.parse(document);
+
+      //         var item = {name: document.meta.ticker.toUpperCase(), data: []};
+      //         var tempData = [];
+      //         for (var stock in document.series)
+      //         {               
+      //           //populate data               
+      //           tempData.push(document.series[stock].close);
+      //           // item.data = tempData;
+      //           // data.push(item);
+      //         }
+      //         item.data = tempData;
+      //         allData.push(item);
+      //       });    
+      //     }
+      //     console.log(allData);
+      //     // document = document.replace('finance_charts_json_callback( ', '');
+      //     // document = document.slice(0, -1);
+      //     // //console.log(document);
+      //     // document = JSON.parse(document);
+
+      //     var cats = [];
+      //     // for (var stock in document.series)
+      //     // {
+      //     //   // for (var d in quotes[stock])
+      //     //   // {
+      //     //     cats.push(document.series[stock]);
+      //     //   // }
+      //     //   break;
+      //     // }
+      //     console.log(allData);
+      //     res.render('stocksView', {title: 'Stocks View', user: req.user, stocks: data, cats: cats });
+      var lastDate = convertDateSubtractDay(date, 1);
+
+        yahooFinance.historical({
+        symbols: s,
+        from: lastDate,
+        to: todaysDate,
+        //TODO: get hourly stocks
+        period: 'd',  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only) 
+      }, function (err, quotes) {
+        if (err)
+          {
+
+          }
+          else
+          {
+            //get categories
+            var cats = [];
+            for (var stock in quotes)
+            {
+              for (var d in quotes[stock])
+              {
+                cats.push(formatDateMMDD(quotes[stock][d].date));
+              }
+              break;
+            }
+            //format data
+            var data = [];
+            for (var stock in quotes)
+            {
+              var item = {name: quotes[stock][0].symbol, data: []};
+              //populate data
+              var tempData = [];
+              for (var d in quotes[stock])
+              {
+                tempData.push(quotes[stock][d].close);
+              }
+              item.data = tempData;
+              data.push(item);
+            }
+            res.render('stocksView', {title: 'Stocks View', user: req.user, stocks: data, ogStocks: s1, cats: cats });
+          } 
+      });
+    }
+    else
+    {
+      var lastDate = convertDateSubtractDay(date, 7);
+
+        yahooFinance.historical({
+        symbols: s,
+        from: lastDate,
+        to: todaysDate,
+        //TODO: get hourly stocks
+        period: 'd',  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only) 
+      }, function (err, quotes) {
+        if (err)
+          {
+
+          }
+          else
+          {
+            //get categories
+            var cats = [];
+            for (var stock in quotes)
+            {
+              for (var d in quotes[stock])
+              {
+                cats.push(formatDateMMDD(quotes[stock][d].date));
+              }
+              break;
+            }
+            //format data
+            var data = [];
+            for (var stock in quotes)
+            {
+              var item = {name: quotes[stock][0].symbol, data: []};
+              //populate data
+              var tempData = [];
+              for (var d in quotes[stock])
+              {
+                tempData.push(quotes[stock][d].close);
+              }
+              item.data = tempData;
+              data.push(item);
+            }
+            res.render('stocksView', {title: 'Stocks View', user: req.user, stocks: data, ogStocks: s1, cats: cats });
+          } 
+      });
+    }
+  }
+  else
+  {
+    res.render('stocksView', {title: 'Stocks View', user: req.user, stocks: s, ogStocks: s, cats: cats  });
+  }
+});
+
+router.get('/stocksView', ensureAuthenticated, function(req, res, next){
+  //default is going to be month
+  var s1 = [];
+
+  if (req.user.stocks.length > 0)
+  {
+    for(var stock in req.user.stocks)
+    {
+      if (isNaN(parseInt(stock)))
+      {    
+      }
+      else
+      {
+        s1.push(req.user.stocks[stock]);
+      }
+    }
+  }
+
+  var s = [];
+  var date = new Date();
+  var todaysDate = convertDate(date);
+  var lastDate = convertDateSubtractMonth(date); 
+
+  if (req.user.stocks.length > 0)
+  {
+    for(var stock in req.user.stocks)
+    {
+      if (isNaN(parseInt(stock)))
+      {    
+      }
+      else
+      {
+        s.push(req.user.stocks[stock].symbol);
+      }
+    }
+
+    yahooFinance.historical({
+      symbols: s,
+      from: lastDate,
+      to: todaysDate,
+      period: 'd',  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only) 
+    }, function (err, quotes) {
+      if (err)
+        {
+
+        }
+        else
+        {
+          //get categories
+          var cats = [];
+          for (var stock in quotes)
+          {
+            for (var d in quotes[stock])
+            {
+              cats.push(formatDateMMDD(quotes[stock][d].date));
+            }
+            break;
+          }
+          //format data
+          var data = [];
+          for (var stock in quotes)
+          {
+            var item = {name: quotes[stock][0].symbol, data: []};
+            //populate data
+            var tempData = [];
+            for (var d in quotes[stock])
+            {
+              tempData.push(quotes[stock][d].close);
+            }
+            item.data = tempData;
+            data.push(item);
+          }
+          res.render('stocksView', {title: 'Stocks View', user: req.user, stocks: data, ogStocks: s1, cats: cats });
+        } 
+    });
+  }
+  else
+  {
+     res.render('stocksView', {title: 'Stocks View', user: req.user, ogStocks: s1, stocks: s });
+  }
 });
 
 router.get('/remove/:sym/:id', (req, res, next) => {
@@ -135,5 +443,65 @@ router.get('/remove/:sym/:id', (req, res, next) => {
   });
   res.redirect('/myStocks');
 });
+
+function convertDate(date) 
+{
+  var yyyy = date.getFullYear().toString();
+  var mm = (date.getMonth()+1).toString();
+  var dd  = date.getDate().toString();
+
+  var mmChars = mm.split('');
+  var ddChars = dd.split('');
+
+  return yyyy + '-' + (mmChars[1]?mm:"0"+mmChars[0]) + '-' + (ddChars[1]?dd:"0"+ddChars[0]);
+}
+
+function convertDateSubtractMonth(date) 
+{
+  var yyyy = date.getFullYear().toString();
+  var mm = (date.getMonth()).toString();
+  var dd  = date.getDate().toString();
+
+  var mmChars = mm.split('');
+  var ddChars = dd.split('');
+
+  return yyyy + '-' + (mmChars[1]?mm:"0"+mmChars[0]) + '-' + (ddChars[1]?dd:"0"+ddChars[0]);
+}
+
+function convertDateSubtractDay(date, days) 
+{
+  var yyyy = date.getFullYear().toString();
+  var mm = (date.getMonth()+1).toString();
+  var dd  = (date.getDate()-days).toString();
+
+  var mmChars = mm.split('');
+  var ddChars = dd.split('');
+
+  return yyyy + '-' + (mmChars[1]?mm:"0"+mmChars[0]) + '-' + (ddChars[1]?dd:"0"+ddChars[0]);
+}
+
+function getDates(startDate, endDate) {
+  var dates = [],
+      currentDate = startDate,
+      addDays = function(days) {
+        var date = new Date(this.valueOf());
+        date.setDate(date.getDate() + days);
+        return date;
+      };
+  while (currentDate <= endDate) {
+    dates.push(currentDate);
+    currentDate = addDays.call(currentDate, 1);
+  }
+  return dates;
+};
+
+function formatDateMMDD(date) 
+{
+  var day = date.getDate()+1;
+  var month = date.getMonth() + 1;
+  var year = date.getFullYear();
+
+  return month + '/' + day;
+}
 
 module.exports = router;
